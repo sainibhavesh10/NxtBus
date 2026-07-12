95804dc4da886f44e77f09dbc13fe17719286473
Bhavesh Saini <sainibhavesh10@gmail.com>
2026-07-12 17:46:24 +0530

    some refactor in tripService

--- backend/src/main/java/com/NxtBus/backend/controller/TripController.java
+++ backend/src/main/java/com/NxtBus/backend/controller/TripController.java
@@ -1,12 +1,17 @@
 package com.nxtbus.backend.controller;
 
+import com.nxtbus.backend.dto.RouteDto;
+import com.nxtbus.backend.dto.TimedStopSequenceDto;
 import com.nxtbus.backend.dto.TripDto;
 import com.nxtbus.backend.response.TripStopSequenceResponse;
+import com.nxtbus.backend.service.RouteService;
 import com.nxtbus.backend.service.TripService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
@@ -15,8 +20,11 @@ public class TripController {
 
     private final TripService tripService;
 
-    public TripController(TripService tripService){
+    private final RouteService routeService;
+
+    public TripController(TripService tripService, RouteService routeService){
         this.tripService = tripService;
+        this.routeService = routeService;
     }
 
     @GetMapping("/{tripId}")
@@ -28,6 +36,18 @@ public class TripController {
     public TripStopSequenceResponse getTripStopSequence(
             @PathVariable String tripId
     ) {
-        return tripService.getTripStopSequence(tripId);
+        TripDto trip = getTripById(tripId);
+        RouteDto route = routeService.getRouteById(trip.routeId());
+
+        List<TimedStopSequenceDto> stops = tripService.getTripStops(tripId);
+
+        return new TripStopSequenceResponse(
+                trip.tripId(),
+                trip.routeId(),
+                route.routeShortName(),
+                trip.serviceId(),
+                trip.tripHeadsign(),
+                stops
+        );
     }
 }
