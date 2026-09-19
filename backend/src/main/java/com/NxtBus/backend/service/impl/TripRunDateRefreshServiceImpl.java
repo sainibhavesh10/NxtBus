431a9b25d052bd3b852cdc60b7505b84dc8ff67b
Bhavesh Saini <sainibhavesh10@gmail.com>
2026-09-19 22:54:53 +0530

    added the TripDateService

--- backend/src/main/java/com/NxtBus/backend/service/impl/TripRunDateRefreshServiceImpl.java
+++ backend/src/main/java/com/NxtBus/backend/service/impl/TripRunDateRefreshServiceImpl.java
@@ -2,8 +2,6 @@ package com.nxtbus.backend.service.impl;
 
 import com.nxtbus.backend.repository.TripRunDateRefreshRepository;
 import com.nxtbus.backend.service.TripRunDateRefreshService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
